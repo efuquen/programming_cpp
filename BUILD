35ce7fdca238253cff75cd5fdda506d0cf8e8c37
Edwin Fuquen <efuquen@gmail.com>
2020-01-01 14:33:50 -0500

    completed some try this exercises in chapter 3 and re-organized files into one workspace

---
+++ BUILD
@@ -0,0 +1,5 @@
+cc_library(
+  name = "stdlib",
+  hdrs = ["std_lib_facilities.h",],
+  visibility = ["//visibility:public"],
+)
---
+++ ch3/unsafe.cpp
@@ -0,0 +1,20 @@
+//
+// Created by efuquen on 1/1/20.
+//
+
+#include "std_lib_facilities.h"
+
+int main () {
+  double d = 0;
+  while(cin>>d) {
+    int i = d;
+    char c = i;
+    int i2 = c;
+    cout << "d==" << d
+         << " i==" << i
+         << " i2==" << i2
+         << " char(" << c << ")\n";
+
+  }
+  return 0;
+}
